2a8da51bae2fefeb9dd96697d48e218d99e088e1
EricThePuffin <143482638+EricThePuffin@users.noreply.github.com>
2023-09-27 12:55:43 +0200

    Update items.py
    Lagd spesifikk rustninger, våpen og bruksgjenstander.

--- items.py
+++ items.py
@@ -9,20 +9,15 @@ class weapon:
     def __init__(self, atk):
         self.atk = atk
 
-class melee(weapon):
+class melee(weapon): #Bare for melee angrep (sverd, spyd, klubber, økser, evt. med fortryllelser på seg).
     def __init__(self, atk):
         super().__init(self, atk)
 
-class ranged(weapon):
-    def __init__(self, atk, range):
-        super().__init__(self, atk)
-        self.range = range
-
-class magic_weapon(weapon):
+class staff(weapon): #Bare for magi-relaterte angrep (spells, o.s.v.), kan muligens sies at kan brukes som en svak klubbe.
     def __init__(self, atk):
         super().__init__(self, atk)
 
-class staff(magic_weapon):
+class ranged(weapon): #Bare for lang distanse angrep (buer, armbrøst)
     def __init__(self, atk):
         super().__init__(self, atk)
 
@@ -30,10 +25,44 @@ class consumable:
     def __init__(self, effect):
         self.effect = effect
 
-class healing_potions(consumable):
-    def __init__(self, effect):
-        super().__init__(effect)
+class potions(consumable): #Gir enten hp eller mana ved bruk.
+    def __init__(self, effect, uses): #Effekt her henviser til hvor mye hp/mana som blir fylt.
+        super().__init__(self, effect)
 
-class mana_potions(consumable):
-    def __init__(self, effect):
-        super().__init__(effect)
+class tomes(consumable): #Brukes for å utføre et magisk angrep uten å selv bruke mana.
+    def __init__(self, effect, uses): #Effekt her henviser til mengde skade.
+        super().__init__(self, effect)
+        self.uses = uses
+
+leather_armor = armor(2)
+chainmail_armor = armor(5)
+iron_armor = armor(7)
+mythril_armor = armor(10)
+
+sword = melee(3)
+spear = melee(5)
+club = melee(2)
+axe = melee(4)
+
+grimoire = staff(3)
+great_staff = staff(5)
+
+shortbow = ranged(3)
+crossbow = ranged(4)
+longbow = ranged(5)
+
+
+lesser_healing_potion = potions(2)
+medium_healing_potion = potions(3)
+greater_healing_potion = potions(5)
+
+lesser_mana_potion = potions(2)
+medium_mana_potion = potions(3)
+greater_mana_potion = potions(5)
+
+lightning_tome = tomes(5, 2)
+fire_tome = tomes(5, 2)
+ice_tome = tomes(5, 2)
+water_tome = tomes(5, 2)
+light_tome = tomes(5, 2)
+dark_tome = tomes(5, 2)
