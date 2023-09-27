1c556eec9c7834d33796928edf20c4089b96046f
EricThePuffin <143482638+EricThePuffin@users.noreply.github.com>
2023-09-27 10:39:30 +0200

    Update items.py
    Skapte grunnbasisen for "items", aka "armor", "weapon" og "consumable", i tillegg er "healing_potions" lagt til.

--- items.py
+++ items.py
@@ -1,3 +1,18 @@
 #############
 ### ITEMS ###
 #############
+class armor:
+    def __init__(self, dfc):
+        self.dfc = dfc
+
+class weapon:
+    def __inint__(self, atk):
+        self.atk = atk
+
+class consumable:
+    def __init__(self, effect):
+        self.effect = effect
+
+class healing_potions(consumable):
+    def __init__(self, effect):
+        super().__init__(effect)
