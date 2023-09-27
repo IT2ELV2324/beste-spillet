67367e0ef875675b527928a5f0b42e40da8cd730
EricThePuffin <143482638+EricThePuffin@users.noreply.github.com>
2023-09-27 11:26:45 +0200

    Update items.py
    Utvidet våpentyper.

--- items.py
+++ items.py
@@ -6,13 +6,34 @@ class armor:
         self.dfc = dfc
 
 class weapon:
-    def __inint__(self, atk):
+    def __init__(self, atk):
         self.atk = atk
+
+class melee(weapon):
+    def __init__(self, atk):
+        super().__init(self, atk)
+
+class ranged(weapon):
+    def __init__(self, atk, range):
+        super().__init__(self, atk)
+        self.range = range
+
+class magic_weapon(weapon):
+    def __init__(self, atk):
+        super().__init__(self, atk)
+
+class staff(magic_weapon):
+    def __init__(self, atk):
+        super().__init__(self, atk)
 
 class consumable:
     def __init__(self, effect):
         self.effect = effect
 
 class healing_potions(consumable):
+    def __init__(self, effect):
+        super().__init__(effect)
+
+class mana_potions(consumable):
     def __init__(self, effect):
         super().__init__(effect)
