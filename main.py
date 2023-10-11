5412237ceaf0e6c96cd57a109734e7bbe67c641d
Sir-Nico <rennemo@proton.me>
2023-10-11 12:36:56 +0200

    ui test

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 #################
 ### MAIN FILE ###
 #################
+from curses import wrapper
+import combat
+import player
+import enemies  # Vet ikke om jeg trenger denne
+import items
+
+
+def main(stdscr):
+    # Clear screen
+    stdscr.clear()
+
+    stdscr.addstr("test")
+
+    stdscr.refresh()
+    stdscr.getkey()
+
+
+if __name__ == "__main__":
+    wrapper(main)
