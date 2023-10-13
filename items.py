d25ec9d101ce51d99ed5197ce1759087517ad2ec
EricThePuffin <143482638+EricThePuffin@users.noreply.github.com>
2023-10-13 15:29:19 +0200

    Update items.py
    Testet litt ting ut, men use() koden fungerer ikke helt enda.

--- items.py
+++ items.py
@@ -11,15 +11,15 @@ class weapon:
 
 class melee(weapon): #Bare for melee angrep (sverd, spyd, klubber, økser, evt. med fortryllelser på seg).
     def __init__(self, atk):
-        super().__init(self, atk)
+        super().__init__(atk)
 
 class staff(weapon): #Bare for magi-relaterte angrep (spells, o.s.v.), kan muligens sies at kan brukes som en svak klubbe.
     def __init__(self, atk):
-        super().__init__(self, atk)
+        super().__init__(atk)
 
 class ranged(weapon): #Bare for lang distanse angrep (buer, armbrøst)
     def __init__(self, atk):
-        super().__init__(self, atk)
+        super().__init__(atk)
 
 class consumable:
     def __init__(self, effect):
@@ -27,19 +27,19 @@ class consumable:
 
 class potions(consumable): #Gir enten hp eller mana ved bruk.
     def __init__(self, effect): #Effekt her henviser til hvor mye hp/mana som blir fylt.
-        super().__init__(self, effect)
+        super().__init__(effect)
 
 class healing_potions(potions): #Gir hp ved bruk.
     def __init__(self, effect): #Effekt her henviser til hvor mye hp som blir fylt.
-        super().__init__(self, effect)
+        super().__init__(effect)
 
 class mana_potions(potions): #Gir mp ved bruk.
     def __init__(self, effect): #Effekt her henviser til hvor mye mp som blir fylt.
-        super().__init__(self, effect)
+        super().__init__(effect)
 
 class tomes(consumable): #Brukes for å utføre et magisk angrep uten å selv bruke mana.
     def __init__(self, effect, uses): #Effekt her henviser til mengde skade.
-        super().__init__(self, effect)
+        super().__init__(effect)
         self.uses = uses
 
 leather_armor = armor(2)
@@ -62,13 +62,13 @@ crossbow = ranged(4)
 longbow = ranged(5)
 
 
-lesser_healing_potion = potions(2)
-medium_healing_potion = potions(3)
-greater_healing_potion = potions(5)
+lesser_healing_potion = healing_potions(2)
+medium_healing_potion = healing_potions(3)
+greater_healing_potion = healing_potions(5)
 
-lesser_mana_potion = potions(2)
-medium_mana_potion = potions(3)
-greater_mana_potion = potions(5)
+lesser_mana_potion = mana_potions(2)
+medium_mana_potion = mana_potions(3)
+greater_mana_potion = mana_potions(5)
 
 lightning_tome = tomes(5, 2)
 fire_tome = tomes(5, 2)
@@ -77,8 +77,9 @@ water_tome = tomes(5, 2)
 light_tome = tomes(5, 2)
 dark_tome = tomes(5, 2)
 
-#Command som skal aktiveres når en item blir brukt.
+#Command som skal aktiveres når en item blir brukt. Fungerer ikke helt enda.
 def use(item):
+    print(item.__class__)
     klasse = item.__class__
     if klasse == healing_potions:
         print("heal")
