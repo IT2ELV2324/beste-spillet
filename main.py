e19a3b29ca4703ac2b5a1d9fa4d2ae00ee268198
Sir-Nico <rennemo@proton.me>
2023-10-16 08:14:02 +0200

    UI test

--- main.py
+++ main.py
@@ -1,21 +1,77 @@
 #################
 ### MAIN FILE ###
 #################
+import curses
+from textcodes import *
 from curses import wrapper
-import combat
-import player
-import enemies  # Vet ikke om jeg trenger denne
-import items
+from UI import *
+# import combat
+# import player
+# import enemies  # Vet ikke om jeg trenger denne
+# import items
 
 
-def main(stdscr):
+def init_ui(stdscr, vars):
+
+    playerName = vars[0]
+    hp = vars[1]
+    areaName = vars[2]
+    message = vars[3]
+
+    stdscr.resize(22, 62)
     # Clear screen
     stdscr.clear()
 
-    stdscr.addstr("test")
+
+    stdscr.addstr(19, 0, "├────────────────────┬───────────────────┬───────────────────┤")
+    stdscr.addstr(20, 1, f"     {playerName}, ")
+    stdscr.addstr(str(hp), curses.COLOR_RED)
+    stdscr.addstr(20, 21, f"│     {areaName}")
+    stdscr.addstr(20, 41, f"│      {message}")
+    stdscr.addstr(21, 0, "└────────────────────┴───────────────────┴───────────────────")
+
+    stdscr.move(18, 1)
 
     stdscr.refresh()
-    stdscr.getkey()
+
+
+def update_ui_info(stdscr, vars):
+
+    playerName = vars[0]
+    hp = vars[1]
+    areaName = vars[2]
+    message = vars[3]
+
+    stdscr.addstr(20, 1, f"     {playerName}, ")
+    stdscr.addstr(str(hp), curses.COLOR_RED)
+    stdscr.addstr(20, 21, f"│     {areaName}")
+    stdscr.addstr(20, 41, f"│      {message}")
+
+
+def reset_cursor(stdscr):
+    stdscr.move(18, 1)
+
+def main(stdscr):
+
+    playerName = "Kjell"
+    hp = 100
+    areaName = "Området"
+    message = "test"
+    
+    init_ui(stdscr, [playerName, hp, areaName, message])
+    stdscr.refresh()
+    
+    is_running = True
+    while is_running:
+
+        # Lat som at det er kode her #
+
+        update_ui_info(stdscr, [playerName, hp, areaName, message])
+        reset_cursor(stdscr)
+        stdscr.refresh()
+        key = stdscr.getkey()
+        if key == "c":
+            is_running = False
 
 
 if __name__ == "__main__":
