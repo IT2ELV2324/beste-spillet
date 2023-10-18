ebb1176f42af9fd0821456a72f3286a7525dc5f1
Lucy Ciara Herud-Thomassen <86323303+LucyCiara@users.noreply.github.com>
2023-10-18 12:18:22 +0200

    ASCII art and screen projection.
    Prints out  a screen-

--- combat.py
+++ combat.py
@@ -4,27 +4,40 @@
 
 #   Imports the curses library for use in terminal formatting.
 import curses
-#   Imports the enemies file
+#   Import the curses library's textpad library.
+import curses.textpad
+#   Imports the enemies file.
 import enemies
-#   Imports the player file
+#   Imports the player file.
 import player
-#   Imports the UI file
+#   Imports the UI file.
 import UI
 
 #   A main function
 def combatMain():
     run = True
     while run:
+        pass
         
 
 
 #   A UI function for preparation of the UI
-def combatUI():
+def combatUI(stdscr):
+    stdscr.clear()
+    stdscr.resize(23, 64)
+    stdscr.border()
+    stdscr.refresh()
+
     #   Creates a window
-    combatWin = curses.newwin(UI.height, UI.width)
+    combatWin = curses.newwin(UI.height, UI.width, 1, 1)
     curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
     
     monsterList = open("MonsterTextArt", "r").readlines()
     for line in monsterList:
         combatWin.addstr(line, curses.color_pair(1))
+
+    combatWin.refresh()
+    combatWin.getkey()
+
+curses.wrapper(combatUI)
     
